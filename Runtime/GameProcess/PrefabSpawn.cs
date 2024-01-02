21dde10bd0c0526bfdf4b5d2b20456caa6a8e086
yusam <yusam@yandex.ru>
2024-01-02 12:14:51 +0400

    #refactring

---
+++ Runtime/GameProcess/PrefabSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace YusamPackage
+{
+    public class PrefabSpawn : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private Transform spawnPoint;
+        private void Awake()
+        {
+            if (prefab != null && spawnPoint != null)
+            {
+                Instantiate(prefab, spawnPoint);
+            } 
+        }
+    }
+}
