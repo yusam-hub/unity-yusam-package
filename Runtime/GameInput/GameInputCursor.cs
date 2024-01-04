0fc9d2c8659cd1df0f9f3c33fb39acc3f396b035
yusam <yusam@yandex.ru>
2024-01-04 13:59:58 +0400

    #changed

---
+++ Runtime/GameInput/GameInputCursor.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YusamPackage
+{
+    public class GameInputCursor : MonoBehaviour
+    {
+        [SerializeField] private Canvas canvas;
+        [SerializeField] private Sprite cursorSprite;
+        [SerializeField] private GameInputCursorUi gameInputCursorUiPrefab;
+        [SerializeField] private Color cursorColor = Color.red;
+
+        private GameInputCursorUi _gameInputCursorUi;
+        private void Awake()
+        {
+            if (gameInputCursorUiPrefab != null && cursorSprite != null && canvas != null)
+            {
+                _gameInputCursorUi = Instantiate(gameInputCursorUiPrefab, canvas.transform);
+                _gameInputCursorUi.gameObject.SetActive(false);
+            }
+        }
+
+        public void DoUpdateImageSpriteColor()
+        {
+            if (_gameInputCursorUi)
+            {
+                _gameInputCursorUi.SetImage(cursorSprite);
+                _gameInputCursorUi.SetImageColor(cursorColor);
+            }
+        }
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                DoUpdateImageSpriteColor();
+            }
+        }
+
+        private void OnEnable()
+        {
+            if (_gameInputCursorUi != null)
+            {
+                DoUpdateImageSpriteColor();
+                _gameInputCursorUi.gameObject.SetActive(true);
+            }
+        }
+        
+        private void OnDisable()
+        {
+            if (_gameInputCursorUi != null)
+            {
+                _gameInputCursorUi.gameObject.SetActive(false);
+            }
+        }
+
+        public RectTransform GetRectTransformCursor()
+        {
+            return _gameInputCursorUi.GetRectTransformCursor();
+
+        }
+        
+        public RectTransform GetRectTransformCanvas()
+        {
+            return canvas.GetComponent<RectTransform>();
+        }
+
+        public Canvas GetCanvas()
+        {
+            return canvas;
+        }
+    }
+}
---
+++ Runtime/GameInput/Ui/GameInputCursorUi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YusamPackage
+{
+    [RequireComponent(typeof(Image))]
+    public class GameInputCursorUi : MonoBehaviour
+    {
+        private RectTransform _rectTransform;
+        private Image _image;
+        private void Awake()
+        {
+            _image = GetComponent<Image>();
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        public void SetImage(Sprite sprite)
+        {
+            _image.sprite = sprite;
+        }
+        
+        public void SetImageColor(Color color)
+        {
+            _image.color = color;
+        }
+
+        public RectTransform GetRectTransformCursor()
+        {
+            return _rectTransform;
+        }
+    }
+}
